abf5d8cf32bc4522d6abb522269250d87ec79b6f
manorajesh <mano@raje.sh>
2023-04-09 22:08:48 -0700

    Separated `update` function into multiple functions

---
+++ README.md
@@ -0,0 +1,2 @@
+# tetris
+Civil-War-Reenactment Style of Tetris 1984 in Rust
--- src/main.rs
+++ src/main.rs
@@ -2,111 +2,31 @@
 // render display vector function
 // update display vector function
 
+// TODO:
+// - state variable not changing after second new piece
+// - right movement not working
+
 use rand::seq::SliceRandom;
+use core::panic;
 use std::{thread::sleep, time::Duration, io::{stdout, Write}};
 
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
+const EMPTY: char = '.';
+
 fn render(display: &Vec<Vec<char>>) {
     print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1)); // clear screen and move cursor to top left
     for row in display {
         for ch in row {
             match ch {
-                '.' => print!(". "),
+                &EMPTY => print!(". "),
                 'a' => print!("[]"),
                 _ => panic!("unknown character: {}", ch),
             }
         }
-        println!();
-    }
-}
-
-fn update(display: &mut Vec<Vec<char>>, state: i32, key: char) -> i32 {
-    let half_width = display[0].len()/2;
-    match state {
-        0 => { // moving
-
-            0
-        },
-
-        1 => { // landed
-            let pieces = vec!['I', 'J', 'L', 'O', 'S', 'T', 'Z'];
-            let piece = pieces.choose(&mut rand::thread_rng()).unwrap();
-            match piece {
-                'I' => {
-                    // I
-                    // I
-                    // I
-                    // I
-                    display[0][half_width] = 'a';
-                    display[1][half_width] = 'a';
-                    display[2][half_width] = 'a';
-                    display[3][half_width] = 'a';
-                }
-                'J' => {
-                    //  J
-                    //  J
-                    // JJ
-                    display[0][half_width] = 'a';
-                    display[1][half_width] = 'a';
-                    display[2][half_width] = 'a';
-                    display[2][half_width-1] = 'a';
-                }
-                'L' => {
-                    // L
-                    // L
-                    // LL
-                    display[0][half_width] = 'a';
-                    display[1][half_width] = 'a';
-                    display[2][half_width] = 'a';
-                    display[2][half_width+1] = 'a';
-                }
-                'O' => {
-                    // OO
-                    // OO
-                    display[0][half_width] = 'a';
-                    display[0][half_width+1] = 'a';
-                    display[1][half_width] = 'a';
-                    display[1][half_width+1] = 'a';
-                }
-                'S' => {
-                    // SS
-                    //  SS
-                    display[0][half_width] = 'a';
-                    display[0][half_width+1] = 'a';
-                    display[1][half_width-1] = 'a';
-                    display[1][half_width] = 'a';
-                }
-                'T' => {
-                    // T
-                    // TT
-                    // T
-                    display[0][half_width] = 'a';
-                    display[1][half_width-1] = 'a';
-                    display[1][half_width] = 'a';
-                    display[1][half_width+1] = 'a';
-                }
-                'Z' => {
-                    //  ZZ
-                    // ZZ
-                    display[0][half_width-1] = 'a';
-                    display[0][half_width] = 'a';
-                    display[1][half_width] = 'a';
-                    display[1][half_width+1] = 'a';
-                }
-                _ => panic!("unknown picece: {}", piece),
-            }
-
-            0 // return to moving state
-        },
-
-        2 => { // game over
-            2
-        },
-        
-        _ => panic!("unknown state: {}", state),
+        print!("\r\n");
     }
 }
 
@@ -116,22 +36,137 @@ fn init(width: i32, height: i32) -> Vec<Vec<char>> {
     for _ in 0..height {
         let mut row: Vec<char> = Vec::new();
         for _ in 0..width {
-            row.push('.');
+            row.push(EMPTY);
         }
         display.push(row);
     }
     display
 }
 
+fn gravity(display: &mut Vec<Vec<char>>) -> i32 {
+    for row in (0..display.len()).rev() {
+        for col in 0..display[row].len() {
+            if display[row][col] == 'a' {
+                if row == display.len()-1 || display[row+1][col] != EMPTY {
+                    return 2; // new piece
+                }
+
+                display[row][col] = EMPTY;
+                display[row+1][col] = 'a';
+            }
+        }
+    }
+    0
+}
+
+fn handle_input(display: &mut Vec<Vec<char>>, key: char) {
+    for row in (0..display.len()).rev() {
+        for col in 0..display[row].len() {
+            if display[row][col] == 'a' {
+                match key {
+                    'l' => {
+                        if col == 0 || display[row][col-1] != EMPTY {
+                            return;
+                        }
+                        display[row][col] = EMPTY;
+                        display[row][col-1] = 'a';
+                    }
+                    'r' => {
+                        if col == display[row].len()-1 || display[row][col+1] != EMPTY {
+                            return;
+                        }
+                        display[row][col] = EMPTY;
+                        display[row][col+1] = 'a';
+                    }
+                    _ => (),
+                }
+            }
+        }
+    }
+}
+
+fn new_piece(display: &mut Vec<Vec<char>>) -> i32 {
+    let half_width = display[0].len() / 2;
+    let pieces = vec!['I', 'J', 'L', 'O', 'S', 'T', 'Z'];
+
+    let piece = pieces.choose(&mut rand::thread_rng()).unwrap();
+    match piece {
+        'I' => {
+            // I
+            // I
+            // I
+            // I
+            display[0][half_width] = 'a';
+            display[1][half_width] = 'a';
+            display[2][half_width] = 'a';
+            display[3][half_width] = 'a';
+        }
+        'J' => {
+            //  J
+            //  J
+            // JJ
+            display[0][half_width] = 'a';
+            display[1][half_width] = 'a';
+            display[2][half_width] = 'a';
+            display[2][half_width-1] = 'a';
+        }
+        'L' => {
+            // L
+            // L
+            // LL
+            display[0][half_width] = 'a';
+            display[1][half_width] = 'a';
+            display[2][half_width] = 'a';
+            display[2][half_width+1] = 'a';
+        }
+        'O' => {
+            // OO
+            // OO
+            display[0][half_width] = 'a';
+            display[0][half_width+1] = 'a';
+            display[1][half_width] = 'a';
+            display[1][half_width+1] = 'a';
+        }
+        'S' => {
+            // SS
+            //  SS
+            display[0][half_width] = 'a';
+            display[0][half_width+1] = 'a';
+            display[1][half_width-1] = 'a';
+            display[1][half_width] = 'a';
+        }
+        'T' => {
+            // T
+            // TT
+            // T
+            display[0][half_width] = 'a';
+            display[1][half_width-1] = 'a';
+            display[1][half_width] = 'a';
+            display[1][half_width+1] = 'a';
+        }
+        'Z' => {
+            //  ZZ
+            // ZZ
+            display[0][half_width-1] = 'a';
+            display[0][half_width] = 'a';
+            display[1][half_width] = 'a';
+            display[1][half_width+1] = 'a';
+        }
+        _ => panic!("unknown picece: {}", piece),
+    }
+
+    0 // return to moving state
+}
+
 fn main() {
     let mut stdin = termion::async_stdin().keys();
-    let stdout = stdout().into_raw_mode().unwrap();
+    let mut stdout = stdout().into_raw_mode().unwrap();
 
-    let width = 15;
-    let height = 20;
-    let mut state = 1; // 0: moving, 1: landed, 2: game over
+    const WIDTH: i32 = 15;
+    const HEIGHT: i32 = 20;
+    let mut state = 2; // 0: moving, 2: new piece
 
-    let mut display: Vec<Vec<char>> = init(width, height);
+    let mut display: Vec<Vec<char>> = init(WIDTH, HEIGHT);
     
     // main loop
     loop {
@@ -147,16 +182,25 @@ fn main() {
             ' '
         };
 
+        // quit
         if key == 'q' {
             break;
         }
 
-        state = update(&mut display, state, key);
+        // spawn new piece
         if state == 2 {
-            break;
+            state = new_piece(&mut display);
         }
+
+        // gravity
+        state = gravity(&mut display);
+
+        // handle input
+        handle_input(&mut display, key);
+
+        // render
         render(&display);
-        stdout.lock().flush().unwrap();
-        sleep(Duration::from_millis(1000));
+        sleep(Duration::from_millis(300));
+        stdout.flush().unwrap();
     }
 }
