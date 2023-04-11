dcf21f669fa914c6948e437912b246f44c44c058
manorajesh <mano@raje.sh>
2023-04-10 20:09:20 -0700

    Gameplay, logic, performance, and aesthetic improvements

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,13 @@
 // TODO:
 // - Fix issue with line clearing
 
-use rand::seq::SliceRandom;
 use core::panic;
-use std::{thread::sleep, time::Duration, io::{stdout, Write}};
+use rand::seq::SliceRandom;
+use std::{
+    io::{stdout, Write},
+    thread::sleep,
+    time::Duration,
+};
 
 use termion::event::Key;
 use termion::input::TermRead;
@@ -15,9 +19,13 @@ use termion::raw::IntoRawMode;
 
 const EMPTY: char = '.';
 
-fn render(display: &Vec<Vec<char>>) {
-    print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1)); // clear screen and move cursor to top left
-    for row in display {
+fn render(display: &Vec<Vec<char>>, is_updated: bool) {
+    if is_updated {
+        return;
+    }
+
+    print!("{}", termion::cursor::Goto(4, 1)); // clear screen and move cursor to top left
+    for (c, row) in display.iter().enumerate() {
         for ch in row {
             match ch {
                 &EMPTY => print!(". "),
@@ -26,13 +34,14 @@ fn render(display: &Vec<Vec<char>>) {
                 _ => panic!("unknown character: {}", ch),
             }
         }
-        print!("\r\n");
+        print!("{}", termion::cursor::Goto(4, (c+2) as u16));
     }
 }
 
 fn init(width: i32, height: i32) -> Vec<Vec<char>> {
     let mut display: Vec<Vec<char>> = Vec::new();
 
+    // generation
     for _ in 0..height {
         let mut row: Vec<char> = Vec::new();
         for _ in 0..width {
@@ -40,6 +49,19 @@ fn init(width: i32, height: i32) -> Vec<Vec<char>> {
         }
         display.push(row);
     }
+
+    // walls
+    print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1)); // clear screen and move cursor to top left
+    for row in &display {
+        print!("<!"); // left wall
+        for _ in row {
+            print!("  ");
+        }
+        print!("!>"); // right wall
+        print!("\r\n");
+    }
+    print!("<!{}!>\r\n", "=".repeat(display[0].len() * 2)); // bottom wall
+    print!("  {}", "\\/".repeat(display[0].len())); // bottom spikes
     display
 }
 
@@ -48,7 +70,7 @@ fn gravity(display: &mut Vec<Vec<char>>) -> bool {
     for row in (0..display.len()).rev() {
         for col in 0..display[row].len() {
             if display[row][col] == 'a' {
-                if row == display.len()-1 || display[row+1][col] != EMPTY {
+                if row == display.len() - 1 || display[row + 1][col] != EMPTY {
                     *display = prev_display;
                     landed(display);
                     let game_over = new_piece(display);
@@ -56,7 +78,7 @@ fn gravity(display: &mut Vec<Vec<char>>) -> bool {
                 }
 
                 display[row][col] = EMPTY;
-                display[row+1][col] = 'a';
+                display[row + 1][col] = 'a';
             }
         }
     }
@@ -70,31 +92,42 @@ fn handle_input(display: &mut Vec<Vec<char>>, key: char) {
             for row in (0..display.len()).rev() {
                 for col in 0..display[row].len() {
                     if display[row][col] == 'a' {
-                        if col == 0 || display[row][col-1] != EMPTY {
+                        if col == 0 || display[row][col - 1] != EMPTY {
                             *display = prev_display;
                             return;
                         }
                         display[row][col] = EMPTY;
-                        display[row][col-1] = 'a';
+                        display[row][col - 1] = 'a';
                     }
                 }
             }
-        },
+        }
 
         'r' => {
             for row in (0..display.len()).rev() {
                 for col in (0..display[row].len()).rev() {
                     if display[row][col] == 'a' {
-                        if col == display[row].len()-1 || display[row][col+1] != EMPTY {
+                        if col == display[row].len() - 1 || display[row][col + 1] != EMPTY {
                             *display = prev_display;
                             return;
                         }
                         display[row][col] = EMPTY;
-                        display[row][col+1] = 'a';
+                        display[row][col + 1] = 'a';
                     }
                 }
             }
-        },
+        }
+
+        's' => {
+            // bring down piece until new piece is created
+            while display[0][display[0].len() / 2] == EMPTY {
+                gravity(display);
+            }
+        }
+
+        'd' => {
+            gravity(display);
+        }
 
         _ => (),
     }
@@ -108,8 +141,8 @@ fn new_piece(display: &mut Vec<Vec<char>>) -> bool {
         return true;
     }
 
-    // let pieces = vec!['I', 'J', 'L', 'O', 'S', 'T', 'Z'];
-    let pieces = vec!['I'];
+    let pieces = vec!['I', 'J', 'L', 'O', 'S', 'T', 'Z'];
+    // let pieces = vec!['I'];
 
     let piece = pieces.choose(&mut rand::thread_rng()).unwrap();
     match piece {
@@ -130,7 +163,7 @@ fn new_piece(display: &mut Vec<Vec<char>>) -> bool {
             display[0][half_width] = 'a';
             display[1][half_width] = 'a';
             display[2][half_width] = 'a';
-            display[2][half_width-1] = 'a';
+            display[2][half_width - 1] = 'a';
         }
         'L' => {
             // L
@@ -139,22 +172,22 @@ fn new_piece(display: &mut Vec<Vec<char>>) -> bool {
             display[0][half_width] = 'a';
             display[1][half_width] = 'a';
             display[2][half_width] = 'a';
-            display[2][half_width+1] = 'a';
+            display[2][half_width + 1] = 'a';
         }
         'O' => {
             // OO
             // OO
             display[0][half_width] = 'a';
-            display[0][half_width+1] = 'a';
+            display[0][half_width + 1] = 'a';
             display[1][half_width] = 'a';
-            display[1][half_width+1] = 'a';
+            display[1][half_width + 1] = 'a';
         }
         'S' => {
             // SS
             //  SS
             display[0][half_width] = 'a';
-            display[0][half_width+1] = 'a';
-            display[1][half_width-1] = 'a';
+            display[0][half_width + 1] = 'a';
+            display[1][half_width - 1] = 'a';
             display[1][half_width] = 'a';
         }
         'T' => {
@@ -162,17 +195,17 @@ fn new_piece(display: &mut Vec<Vec<char>>) -> bool {
             // TT
             // T
             display[0][half_width] = 'a';
-            display[1][half_width-1] = 'a';
+            display[1][half_width - 1] = 'a';
             display[1][half_width] = 'a';
-            display[1][half_width+1] = 'a';
+            display[1][half_width + 1] = 'a';
         }
         'Z' => {
             //  ZZ
             // ZZ
-            display[0][half_width-1] = 'a';
+            display[0][half_width - 1] = 'a';
             display[0][half_width] = 'a';
             display[1][half_width] = 'a';
-            display[1][half_width+1] = 'a';
+            display[1][half_width + 1] = 'a';
         }
         _ => panic!("unknown picece: {}", piece),
     }
@@ -197,7 +230,7 @@ fn full_line(display: &mut Vec<Vec<char>>) {
             }
         }
         display.remove(row);
-        display.insert(row, vec![EMPTY; display[0].len()]); // insert empty row at idx
+        display.insert(0, vec![EMPTY; display[0].len()]); // add new line at the top
     }
 }
 
@@ -212,15 +245,20 @@ fn main() {
 
     print!("{}", termion::cursor::Hide);
     new_piece(&mut display);
-    
+
+    let mut counter: usize = 0;
+
     // main loop
     loop {
+        let prev_display = display.clone();
         // handle input
         let key = if let Some(Ok(key)) = stdin.next() {
             match key {
-                Key::Char('q') => 'q',
-                Key::Left => 'l',
-                Key::Right => 'r',
+                Key::Char('q') => 'q', // quit
+                Key::Left => 'l',      // left
+                Key::Right => 'r',     // right
+                Key::Char(' ') => 's', // down with spacebar
+                Key::Down => 'd',      // down
                 _ => ' ',
             }
         } else {
@@ -233,8 +271,10 @@ fn main() {
         }
 
         // gravity
-        if gravity(&mut display) {
-            break;
+        if counter % 4 == 0 {
+            if gravity(&mut display) {
+                break;
+            }
         }
 
         // handle input
@@ -243,11 +283,15 @@ fn main() {
         // full line
         full_line(&mut display);
 
+        // check if display was changed
+        let is_updated = display != prev_display;
+
         // render
-        render(&display);
+        render(&display, is_updated);
+        sleep(Duration::from_millis(50));
         stdout.flush().unwrap();
-        sleep(Duration::from_millis(150));
+        counter += 1;
     }
 
-    print!("{}", termion::cursor::Show);
+    print!("{}\r\n\r\n\r\n", termion::cursor::Show);
 }
