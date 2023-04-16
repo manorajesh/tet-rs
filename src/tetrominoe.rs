db797d5a6b80e90862653d82488e1583ffb2608c
manorajesh <mano@raje.sh>
2023-04-15 23:19:05 -0700

    Fixed issue with `s` and `z` pieces' rotations

--- src/tetrominoe.rs
+++ src/tetrominoe.rs
@@ -5,7 +5,8 @@ pub struct Tetrominoe {
     pub shape: Vec<Vec<char>>,
     pub row: usize,
     pub col: usize,
-    ptype: char
+    ptype: char,
+    state: usize,
 }
 
 impl Tetrominoe {
@@ -14,7 +15,8 @@ impl Tetrominoe {
             shape: Vec::new(),
             row: 0,
             col: 0,
-            ptype: ' '
+            ptype: ' ',
+            state: 0,
         }
     }
 
@@ -41,7 +43,7 @@ impl Tetrominoe {
                         vec![EMP, 'a', 'a', EMP],
                         vec![EMP, EMP, EMP, EMP]],
 
-            'S' => vec![vec![EMP, EMP, EMP, EMP],
+            'Z' => vec![vec![EMP, EMP, EMP, EMP],
                         vec!['a', 'a', EMP, EMP],
                         vec![EMP, 'a', 'a', EMP],
                         vec![EMP, EMP, EMP, EMP]],
@@ -51,7 +53,7 @@ impl Tetrominoe {
                         vec!['a', 'a', 'a', EMP],
                         vec![EMP, EMP, EMP, EMP]],
                         
-            'Z' => vec![vec![EMP, EMP, EMP, EMP],
+            'S' => vec![vec![EMP, EMP, EMP, EMP],
                         vec![EMP, 'a', 'a', EMP],
                         vec!['a', 'a', EMP, EMP],
                         vec![EMP, EMP, EMP, EMP]],
@@ -59,6 +61,7 @@ impl Tetrominoe {
             _ => panic!("Unknown shape: {}", shape),
         };
         self.shape = shape;
+        self.state = 0;
     }
 
     pub fn set_pos(&mut self, row: usize, col: usize) {
@@ -67,20 +70,60 @@ impl Tetrominoe {
     }
 
     pub fn rotate(&mut self) {
-         // transpose or swap rows and columns
-         let n = self.shape.len();
-         for i in 0..n {
-             for j in i..n {
-                 let temp = self.shape[i][j];
-                 self.shape[i][j] = self.shape[j][i];
-                 self.shape[j][i] = temp;
-             }
-         }
-         
-         // reverse each row to rotate
-         for i in 0..n {
-             self.shape[i].reverse();
-         }
+        match self.ptype {
+            'O' => (),
+            'I' | 'J' | 'L' | 'T' => {
+                // transpose or swap rows and columns
+                let n = self.shape.len();
+                for i in 0..n {
+                    for j in i..n {
+                        let temp = self.shape[i][j];
+                        self.shape[i][j] = self.shape[j][i];
+                        self.shape[j][i] = temp;
+                    }
+                }
+                
+                // reverse each row to rotate
+                for i in 0..n {
+                    self.shape[i].reverse();
+                }
+            }
+
+        'Z' => {
+            if self.state == 0 {
+                self.shape = vec![vec![EMP, EMP, EMP, EMP],
+                                  vec![EMP, EMP, 'a', EMP],
+                                  vec![EMP, 'a', 'a', EMP],
+                                  vec![EMP, 'a', EMP, EMP]];
+                self.state = 1;
+            } else {
+                self.shape = vec![vec![EMP, EMP, EMP, EMP],
+                                  vec!['a', 'a', EMP, EMP],
+                                  vec![EMP, 'a', 'a', EMP],
+                                  vec![EMP, EMP, EMP, EMP]];
+                self.state = 0;
+            }
+        }
+
+        'S' => {
+            if self.state == 0 {
+                self.shape = vec![vec![EMP, EMP, EMP, EMP],
+                                  vec![EMP, 'a', EMP, EMP],
+                                  vec![EMP, 'a', 'a', EMP],
+                                  vec![EMP, EMP, 'a', EMP]];
+                
+                self.state = 1;
+            } else {
+                self.shape = vec![vec![EMP, EMP, EMP, EMP],
+                                  vec![EMP, 'a', 'a', EMP],
+                                  vec!['a', 'a', EMP, EMP],
+                                  vec![EMP, EMP, EMP, EMP]];
+                self.state = 0;
+            }
+        }
+
+        _ => panic!("Unknown shape: {}", self.ptype),
     }
+}
     
 }
